30d0e96608100a6471a98eb5dd13ad797b0b9e98
oubenali mohamed <oubenali15@gmail.com>
2022-07-28 17:48:58 +0200

    ajout du style au composant error

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,11 +1,54 @@
 import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
 
+const StyleSpan = styled.span`
+position: absolute;
+left: 35%;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 288px;
+line-height: 142.6%;
+display: flex;
+align-items: center;
+text-align: center;
+color: #FF6060;
+`
+const StyleTitre = styled.h1`
+position: absolute;
+left: 25%;
+top: 65%;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 36px;
+line-height: 142.6%;
+display: flex;
+align-items: flex-end;
+color: #FF6060;
+`
+const StyleLink = styled(NavLink)`
+position: absolute;
+left: 40.14%;
+right: 40.14%;
+top: 81.93%;
+bottom: 15.53%;
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 142.6%;
+display: flex;
+align-items: flex-end;
+text-decoration-line: underline;
+color: #FF6060;
+`
 function Error() {
   return (
     <div>
-      <span>404</span>
-      <h1>Oups! La page que vous demandez n'existe pas</h1>
-      <NavLink to="/">Retourner sur la page d’accueil</NavLink>
+      <StyleSpan>404</StyleSpan>
+      <StyleTitre>Oups! La page que vous demandez n'existe pas</StyleTitre>
+      <StyleLink to="/">Retourner sur la page d’accueil</StyleLink>
     </div>
   )
 }
